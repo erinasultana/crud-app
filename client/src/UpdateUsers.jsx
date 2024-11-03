98c11c942bf404462eed646454b5ab6ffa85d801
erinasultana <erinasultanaeva@gmail.com>
2024-11-03 12:22:37 +0600

    second

--- client/src/UpdateUsers.jsx
+++ client/src/UpdateUsers.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function UpdateUsers  ()  {
+  const {id} = useParams()
+  const [name,setName] = useState()
+  const [email,setEmail] = useState()
+  const [age,setAge] = useState()
+  const navigate = useNavigate()
+
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/getUser/'+id)
+    .then(result => {console.log(result)
+         setName(result.data.name)
+         setEmail(result.data.email)
+         setAge(result.data.age)
+
+  })
+    .catch(err => console.log(err))
+  },[])
+
+
+  const Update = (e) => {
+    e.preventDefault();
+    axios.put("http://localhost:3001/updateUser/"+id,{name,email,age})
+    .then(result => {
+      console.log(result)
+      navigate('/')
+    })
+    .catch(err => console.log(err))
+
+  }
+
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center   align-items-center">
       <div className="w-50 bg-white rounded p-3">
-        <form>
+        <form onSubmit={Update}>
           <h2>Update User</h2>
           <div className="mb-2">
             <label htmlFor=" ">Name</label>
@@ -12,6 +44,8 @@ function UpdateUsers  ()  {
               type="text"
               className="form-control"
               placeholder="Enter Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="mb-2">
@@ -20,6 +54,8 @@ function UpdateUsers  ()  {
               type="email"
               className="form-control"
               placeholder="Enter Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mb-2">
@@ -28,6 +64,8 @@ function UpdateUsers  ()  {
               type="text"
               className="form-control"
               placeholder="Enter Age"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
             />
           </div>
           <button className="btn btn-success">Update</button>
--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -1,21 +1,33 @@
 import axios from "axios";
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-//import { useState } from 'react';
 
-function Users () {
-  const [users, setUsers] = useState([])
+function Users() {
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3001')
-    .then(result => setUsers(result.data))
-    .catch(err => console.log(err))
-  },[])
+    axios
+      .get("http://localhost:3001")
+      .then((result) => setUsers(result.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleDelete = (id) => {
+    axios
+      .delete("http://localhost:3001/deleteUser/" + id)
+      .then((res) => {
+        console.log(res);
+        window.location.reload();
+      })
+      .catch((errr) => console.log(errr));
+  };
 
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
       <div className="w-50 bg-white rounded p-3">
-        <Link to="/create" className="btn btn-primary">Add+</Link>
+        <Link to="/create" className="btn btn-primary">
+          Add+
+        </Link>
         <table className="table">
           <thead>
             <tr>
@@ -27,15 +39,28 @@ function Users () {
           </thead>
           <tbody>
             {users.map((user) => {
-              return <tr>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                <td>{user.age}</td>
-                <td>
-                <Link to="/update" className="btn btn-success">Update</Link>
-                <button type="button" className="btn btn-danger">delete</button>
-                </td>
-              </tr>;
+              return (
+                <tr>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.age}</td>
+                  <td>
+                    <Link
+                      to={`/update/${user._id}`}
+                      className="btn btn-success"
+                    >
+                      Update
+                    </Link>
+                    <button
+                      type="button"
+                      className="btn btn-danger"
+                      onClick={(e) => handleDelete(user._id)}
+                    >
+                      delete
+                    </button>
+                  </td>
+                </tr>
+              );
             })}
           </tbody>
         </table>
